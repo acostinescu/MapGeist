01c930b8a81f69127c84b4fca3e1cc8dc10663ea
Alex Costinescu <paul.costinescu@gmail.com>
2019-11-12 16:59:48 -0700

    Update PublicController.java

--- CrunchifySpringMVCTutorial/src/mapGeist/PublicController.java
+++ CrunchifySpringMVCTutorial/src/mapGeist/PublicController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
  
-/*
- * author: Crunchify.com
- * 
- */
- 
 @Controller
 public class PublicController {
  
